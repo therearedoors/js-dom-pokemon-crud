e948ccb51e7a609c65e782f8f80faf9afd4e0e90
Paul Smart <pdsmart6@gmail.com>
2022-02-16 16:32:28 +0000

    delete implemented

--- src/index.js
+++ src/index.js
@@ -2,21 +2,35 @@ const pokeForm = document.querySelector(".poke-form");
 const pokeList = document.querySelector(".poke-list");
 
 function addPokemon(pokemon) {
+  const div = document.createElement("div");
   const liEl = document.createElement("li");
   const imgEl = document.createElement("img");
   const h2El = document.createElement("h2");
+  const btn = document.createElement("button")
 
   liEl.classList.add("pokemon");
   imgEl.src = pokemon.image;
 
   h2El.innerText = pokemon.name;
 
+  btn.addEventListener("click", function(){deleteFromDatabase(pokemon.id)})
+  btn.innerText = "yeet me"
+
   liEl.append(imgEl, h2El);
-  pokeList.append(liEl);
+  div.append(liEl, btn)
+  pokeList.append(div);
 }
 
 function addPokemons(pokemons) {
   pokemons.forEach(pokemon => addPokemon(pokemon))
+}
+
+function deleteFromDatabase(id){
+  fetch(`http://localhost:3000/pokemon/${id}`, {
+    method: "DELETE"
+  })
+  //.then(res =>  res.json())
+  //.then(pokemon => addPokemon(pokemon));
 }
 
 function listenToAddPokemonForm() {
@@ -26,30 +40,30 @@ function listenToAddPokemonForm() {
       name: pokeForm.name.value,
       image: pokeForm.image.value
     };
-
     // CREATE
-    // fetch("http://localhost:3000/pokemons", {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json"
-    //   },
-    //   body: JSON.stringify(pokemon)
-    // })
-    //   .then(res =>  res.json())
-    //   .then(pokemon => addPokemon(pokemon));
-    //   });
+     fetch("http://localhost:3000/pokemon", {
+       method: "POST",
+       headers: {
+         "Content-Type": "application/json"
+       },
+       body: JSON.stringify(pokemon)
+     })
+       .then(res =>  res.json())
+       .then(pokemon => addPokemon(pokemon));
 
-    pokeForm.reset();
+    //pokeForm.reset();
   });
 }
 
 function init() {
   listenToAddPokemonForm();
-
   // READ
-  // fetch("http://localhost:3000/pokemons")
-  //   .then(res => res.json());
-  //   .then(pokemons => addPokemons(pokemons));
+  fetch("http://localhost:3000/pokemon")
+    .then(res => res.json())
+    .then(
+      p => {
+        addPokemons(p)
+      });
 }
 
 init();
